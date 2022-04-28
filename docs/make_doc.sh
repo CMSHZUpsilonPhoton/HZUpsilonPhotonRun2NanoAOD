67a28bb94322cbedc6a9111e484294c5eb57a3f8
Felipe Silva <felipe.silva@cern.ch>
2022-04-27 22:52:20 -0400

    which conda

--- docs/make_doc.sh
+++ docs/make_doc.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
 conda create -y -c conda-forge -p ./HZUpsilonPhotonRun2NanoAOD_env python=3.9.12 mamba
+
+which conda
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/ftorresd/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/ftorresd/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/ftorresd/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/ftorresd/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
 conda activate ./HZUpsilonPhotonRun2NanoAOD_env
 mamba install -y -c conda-forge --file environment.yml
 pdoc hzupsilonphoton --math -o ./docs
